15fb55ec76a8927b9e7c03d72ce3075535a6857d
nirenrawal <nire0007@stud.kea.dk>
2022-12-15 00:10:21 +0100

    delete profile, interface, photo upload bug fix

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 $_title = 'Home';
-require_once __DIR__.'/template/header.php';
+require_once __DIR__ . '/template/header.php';
 $student = new Students();
 $a = $student->profilePage();
 // $d = $student->delProfile();
@@ -9,8 +9,6 @@ $a = $student->profilePage();
 $dob = new DateTime($a[0]->dob);
 $today   = new DateTime('today');
 $year = $dob->diff($today)->y;
-
-
 ?>
 
 <div class="main-content">
@@ -32,90 +30,61 @@ $year = $dob->diff($today)->y;
                     </div>
                     <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
                         <div class="d-flex justify-content-between">
+
                             <a type="button" onclick="window.location='update-profile.php';" class="btn btn-sm btn-info mr-4">Edit Profile</a>
-                            <input type="submit" name='del' class="btn btn-sm btn-danger float-right" value="Delete Profile<?php //echo $student->delProfile() ?>" onclick="return confirm('Are you sure you want to delete your account permanently?');"/>
-                            <!-- <button type="button" class="btn btn-sm btn-danger float-right" data-toggle="modal" data-target="#exampleModalLong"> 
-                                Delete Profile
-                            </button> -->
-                            
-                    </div>
-                    <div class="card-body pt-0 pt-md-4">
-                        <div class="row">
-                            <div class="col">
-                                <div class="card-profile-stats d-flex justify-content-center mt-md-5">
-                                    <div>
-                                        <span class="heading"><?php echo(rand(1,5000));?></span>
-                                        <span class="description">Friends</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading"><?php echo(rand(1,50));?></span>
-                                        <span class="description">Photos</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading"><?php echo(rand(1,200));?></span>
-                                        <span class="description">Comments</span>
+
+                            <form action='<?php $student->delete() ?>' method='post'>
+                                <input type='hidden' name='id' value='<?php echo $a[0]->id; ?>'>
+                                <input class="btn btn-sm btn-danger float-right" type='submit' name='delete' value='Delete Profile' onclick="return confirm('Are you sure, you want to delete your profile?');">
+                            </form>
+
+                        </div>
+                        <div class="card-body pt-0 pt-md-4">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="card-profile-stats d-flex justify-content-center mt-md-5">
+                                        <div>
+                                            <span class="heading"><?php echo (rand(1, 5000)); ?></span>
+                                            <span class="description">Friends</span>
+                                        </div>
+                                        <div>
+                                            <span class="heading"><?php echo (rand(1, 50)); ?></span>
+                                            <span class="description">Photos</span>
+                                        </div>
+                                        <div>
+                                            <span class="heading"><?php echo (rand(1, 200)); ?></span>
+                                            <span class="description">Comments</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="text-center">
-                            <h3>
-                                <?php echo $a[0]->name ?>
-                                <span class="font-weight-light"></span>
-                            </h3>
-                            <h5><span class="font-weight-light">Age: </span><?php echo $year ?></h5>
-                            <div class="h5 font-weight-300">
-                                <i class="ni location_pin mr-2"></i><?php echo $a[0]->address ?>
+                            <div class="text-center">
+                                <h3>
+                                    <?php echo $a[0]->name ?>
+                                    <span class="font-weight-light"></span>
+                                </h3>
+                                <h5><span class="font-weight-light">Age: </span><?php echo $year ?></h5>
+                                <div class="h5 font-weight-300">
+                                    <i class="ni location_pin mr-2"></i><?php echo $a[0]->address ?>
+                                </div>
+                                <div class="h5 mt-4">
+                                    <i class="ni business_briefcase-24 mr-2"></i><?php echo $a[0]->faculty ?>
+                                </div>
+                                <div>
+                                    <i class="ni education_hat mr-2"></i><?php echo $a[0]->school ?>
+                                </div>
+                                <hr class="my-4">
+                                <p><?php echo $a[0]->about ?></p>
+                                <a href="https://www.wikipedia.org" target="_blank">Show more</a>
                             </div>
-                            <div class="h5 mt-4">
-                                <i class="ni business_briefcase-24 mr-2"></i><?php echo $a[0]->faculty ?>
-                            </div>
-                            <div>
-                                <i class="ni education_hat mr-2"></i><?php echo $a[0]->school ?>
-                            </div>
-                            <hr class="my-4">
-                            <p><?php echo $a[0]->about ?></p>
-                            <a href="https://www.wikipedia.org" target="_blank">Show more</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-
-
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Delete Profile</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="form-group">
-            <label for="password">Enter password to delete your profile.</label>
-            <input name="password" type="password" class="form-control" id="password" placeholder="Enter Password">
-          </div>
-      </div>
-      <div class="modal-footer">
-        <form action="del-p.php" method="POST">
-        <input type="button" class="btn btn-sm btn-secondary mr-4" data-dismiss="modal" value="Close">
-        <input type='hidden' name='password' value='<?php echo $student->id; ?>'>
-        <input class="btn btn-sm btn-danger mr-4" type='submit' name='delete-profile' value='Delete Profile'>
-        <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-danger">Delete</button> -->
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
 
-<?php require_once __DIR__.'/template/footer.php';?>
+    <?php require_once __DIR__ . '/template/footer.php'; ?>
